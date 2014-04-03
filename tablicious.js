31fe08670e4ab2d4cc4b6b22454828a193ffdf03
Shayne Vacher <svacher@daptiv.com>
2014-04-03 15:11:36 -0700

    continued refactor and fixed default tab

--- tablicious.js
+++ tablicious.js
@@ -1,52 +1,73 @@
 var tablicious = function (options){
     var opts = options || {};
+    var tabCssClasses = opts.tabCssClasses || {};
     var tabHeaderContainerId = opts.tabHeaderContainerId || 'tabs';
     var tabContentContainerId = opts.tabContentContainerId || 'tabsContent';
     var tabHeaderPrefix = opts.tabHeaderPrefix || 'tabHeader';
     var tabPagePrefix = opts.tabPagePrefix || 'tabpage';
-    var initalStartingTabNumber = opts.initalStartingTabNumber || 1;
+    var initialStartingTabNumber = opts.initialStartingTabNumber || 1;
+
+    tabCssClasses.inactiveHeader = tabCssClasses.inactiveHeader || 'tabInactiveHeader';
+    tabCssClasses.inactivePage = tabCssClasses.inactivePage || 'tabInactivePage';
+    tabCssClasses.activeHeader = tabCssClasses.activeHeader || 'tabActiveHeader';
+    tabCssClasses.activePage = tabCssClasses.activePage || 'tabActivePage';
 
     window.onload = function init() {
-        var tabContainer = document.getElementById(tabHeaderContainerId);
+
         var tabContent = document.getElementById(tabContentContainerId);
-
-        setActiveTab(initalStartingTabNumber);
-
         var pages = tabContent.getElementsByTagName('div');
 
-        for (var i = 1; i < pages.length; i++) {
+        for (var i = 0; i < pages.length; i++) {
             hideInactiveTab(getTabNumber(pages.item(i)));
         }
+        setActiveTab(initialStartingTabNumber);
 
-        var tabs = tabContainer.getElementsByTagName('li');
-        for (var i = 0; i < tabs.length; i++) {
-            tabs[i].onclick = displayPage;
-        }
+        actOnAllTabs(function(tabs, currentIndex) {
+            tabs[currentIndex].onclick = displayPage;
+        });
     }
 
+    var actOnAllTabs = function( action ){
+        var tabContainer = document.getElementById(tabHeaderContainerId);
+        var tabs = tabContainer.getElementsByTagName('li');
+        for (var i = 0; i < tabs.length; i++) {
+            action(tabs, i);
+        }
+    };
+
     var hideInactiveTab = function (tabNumber) {
-        document.getElementById(getTabElementId(tabHeaderPrefix,tabNumber)).setAttribute('class', 'tabInactiveHeader');
-        document.getElementById(getTabElementId(tabPagePrefix,tabNumber)).setAttribute('class', 'tabInactivePage');
+        document.getElementById(getTabElementId(tabHeaderPrefix,tabNumber)).setAttribute('class', tabCssClasses.inactiveHeader);
+        document.getElementById(getTabElementId(tabPagePrefix,tabNumber)).setAttribute('class', tabCssClasses.inactivePage);
     };
 
     var setActiveTab = function (tabNumber) {
         var tab = document.getElementById(getTabElementId(tabHeaderPrefix,tabNumber));
-        tab.setAttribute('class', 'tabActiveHeader');
+        tab.setAttribute('class', tabCssClasses.activeHeader);
 
-        document.getElementById(getTabElementId(tabPagePrefix,tabNumber)).setAttribute('class', 'tabActivePage');
-        tab.parentNode.setAttribute('data-active-tab', tabNumber);
+        document.getElementById(getTabElementId(tabPagePrefix,tabNumber)).setAttribute('class', tabCssClasses.activePage);
     };
 
     var getTabNumber = function (tabElement){
         return tabElement.id.split('_')[1];
     };
 
-    var getTabElementId = function(tabPrefix, tabId){
-        return tabPrefix + '_' + tabId;
+    var getTabElementId = function(tabPrefix, tabNumber){
+        return tabPrefix + '_' + tabNumber;
+    };
+
+    var getActiveTab = function() {
+        var activeTab;
+        actOnAllTabs( function(tabs, currentIndex){
+            if (tabs[currentIndex].getAttribute('class') === tabCssClasses.activeHeader){
+                activeTab = tabs[currentIndex];
+            }
+        });
+        return activeTab;
     };
 
     var displayPage = function () {
-        var activeTabNumber = this.parentNode.getAttribute('data-active-tab');
+
+        var activeTabNumber = getTabNumber(getActiveTab());
         hideInactiveTab(activeTabNumber);
 
         var tabNumber = getTabNumber(this);
@@ -54,4 +75,4 @@ var tablicious = function (options){
     };
 };
 
-tablicious({});
+tablicious();
