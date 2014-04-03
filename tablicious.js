ba0c8feaaf06075a14ed2436459b7e37b33fcc3d
Shayne Vacher <svacher@daptiv.com>
2014-04-03 14:21:30 -0700

    made closure, and configurable

--- tablicious.js
+++ tablicious.js
@@ -1,48 +1,57 @@
-window.onload = function init() {
-    var tabContainer = document.getElementById('tabs');
-    var tabContent = document.getElementById('tabsContent');
-    var currentTab = document.getElementById('tabHeader_1');
+var tablicious = function (options){
+    var opts = options || {};
+    var tabHeaderContainerId = opts.tabHeaderContainerId || 'tabs';
+    var tabContentContainerId = opts.tabContentContainerId || 'tabsContent';
+    var tabHeaderPrefix = opts.tabHeaderPrefix || 'tabHeader';
+    var tabPagePrefix = opts.tabPagePrefix || 'tabpage';
+    var initalStartingTabNumber = opts.initalStartingTabNumber || 1;
 
-    var activePageTabId = getTabId( currentTab );
-    setActiveTab(activePageTabId);
+    window.onload = function init() {
+        var tabContainer = document.getElementById(tabHeaderContainerId);
+        var tabContent = document.getElementById(tabContentContainerId);
 
-    var pages = tabContent.getElementsByTagName('div');
+        setActiveTab(initalStartingTabNumber);
 
-    for (var i = 1; i < pages.length; i++) {
-        hideInactiveTab(getTabId(pages.item(i)));
+        var pages = tabContent.getElementsByTagName('div');
+
+        for (var i = 1; i < pages.length; i++) {
+            hideInactiveTab(getTabNumber(pages.item(i)));
+        }
+
+        var tabs = tabContainer.getElementsByTagName('li');
+        for (var i = 0; i < tabs.length; i++) {
+            tabs[i].onclick = displayPage;
+        }
     }
 
-    var tabs = tabContainer.getElementsByTagName('li');
-    for (var i = 0; i < tabs.length; i++) {
-      tabs[i].onclick = displayPage;
-    }
-}
+    var hideInactiveTab = function (tabNumber) {
+        document.getElementById(getTabElementId(tabHeaderPrefix,tabNumber)).setAttribute('class', 'tabInactiveHeader');
+        document.getElementById(getTabElementId(tabPagePrefix,tabNumber)).setAttribute('class', 'tabInactivePage');
+    };
 
-function hideInactiveTab(tabId) {
-    document.getElementById('tabHeader_' + tabId).setAttribute('class', 'tabInactiveHeader');
-    document.getElementById('tabpage_' + tabId).setAttribute('class', 'tabInactivePage');
-}
+    var setActiveTab = function (tabNumber) {
+        var tab = document.getElementById(getTabElementId(tabHeaderPrefix,tabNumber));
+        tab.setAttribute('class', 'tabActiveHeader');
 
-function setActiveTab(tabId) {
-    var tab = getTabElementById(tabId);
-    tab.setAttribute('class', 'tabActiveHeader');
+        document.getElementById(getTabElementId(tabPagePrefix,tabNumber)).setAttribute('class', 'tabActivePage');
+        tab.parentNode.setAttribute('data-active-tab', tabNumber);
+    };
 
-    document.getElementById('tabpage_' + tabId).setAttribute('class', 'tabActivePage');
-    tab.parentNode.setAttribute('data-active-tab', tabId);
-}
+    var getTabNumber = function (tabElement){
+        return tabElement.id.split('_')[1];
+    };
 
-function displayPage() {
-    var activeTabId = this.parentNode.getAttribute('data-active-tab');
-    hideInactiveTab(activeTabId);
+    var getTabElementId = function(tabPrefix, tabId){
+        return tabPrefix + '_' + tabId;
+    };
 
-    var tabId = getTabId(this);
-    setActiveTab(tabId);
-}
+    var displayPage = function () {
+        var activeTabNumber = this.parentNode.getAttribute('data-active-tab');
+        hideInactiveTab(activeTabNumber);
 
-function getTabId(tabElement){
-    return tabElement.id.split('_')[1];
-}
+        var tabNumber = getTabNumber(this);
+        setActiveTab(tabNumber);
+    };
+};
 
-function getTabElementById(tabId){
-    return document.getElementById('tabHeader_' + tabId);
-}
+tablicious({});
