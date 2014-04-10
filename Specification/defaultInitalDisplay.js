c00f8b1759cc845f21b0b172cbfcf7d184a37ce6
Shayne Vacher <svacher@daptiv.com>
2014-04-09 17:02:22 -0700

    added unit testing through jasmine and npm install info

---
+++ Specification/defaultInitalDisplay.js
@@ -0,0 +1,13 @@
+describe("when running tests", function (){
+    beforeEach( function () {
+
+    });
+
+    afterEach( function (){
+
+    });
+
+   it("should run the tests", function (){
+       expect(true).toEqual(false);
+   });
+});
