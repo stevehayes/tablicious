cfc0942188e8d6080018744ac6894b4c43487b4b
Shayne Vacher <svacher@daptiv.com>
2014-04-10 10:36:21 -0700

    got the first basic test wired up

--- Specification/defaultInitalDisplay.js
+++ Specification/defaultInitalDisplay.js
@@ -1,13 +1,24 @@
-describe("when running tests", function (){
+describe('rendering tabs with default settings', function (){
     beforeEach( function () {
-
+        setFixtures(sampleHtml.basic);
+        tablicious().initializeTabs();
     });
 
     afterEach( function (){
-
+        jasmine.getFixtures().cleanUp();
     });
 
-   it("should run the tests", function (){
-       expect(true).toEqual(false);
+   it('should select only the first tab', function (){
+       expect($('#tabHeader_1')).toHaveClass('tabActiveHeader');
+       expect($('#tabHeader_2')).toHaveClass('tabInactiveHeader');
    });
+
+   it('should show the first tabs contents', function(){
+       expect($('#tabpage_1')).toHaveClass('tabActivePage');
+   });
+
+   it('should hide all other tabs contents', function(){
+       expect($('#tabpage_2')).toHaveClass('tabInactivePage');
+   });
+
 });
